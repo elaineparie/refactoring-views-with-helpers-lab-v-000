5b1966209152ce1861920109dbcbd806b94a7685
Elaine Anderson <eparie@bu.edu>
2018-08-07 12:21:21 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,7 +9,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    self.artist: Artist.find_or_create_by(name: name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 
 
