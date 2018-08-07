0d4e9d47c8e80a18feff4f6bc0b3387ead16e078
Elaine Anderson <eparie@bu.edu>
2018-08-07 11:51:15 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,8 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    #  self.artist = Artist.find_or_create_by(name: name)
+    binding.pry
+     self.artist = Artist.find_or_create_by(name: name)
   end
 
   def artist_name=(name)
