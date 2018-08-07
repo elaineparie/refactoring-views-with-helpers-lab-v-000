37d2e080f7e5e8030e42c0c646c5f65b4811994c
Elaine Anderson <eparie@bu.edu>
2018-08-07 11:39:01 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,6 +2,8 @@ module ArtistsHelper
   def display_artist(song)
     if !song.artist
       link_to 'Add Artist', "/songs/#{song.id}/edit"
+    else
+      redirect_to "/artists/#{song.artists.id}"
     end
 
   end
