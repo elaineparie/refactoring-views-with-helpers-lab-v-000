e923d9d6a55ba17914c8500f348059e1a4e962b4
Elaine Anderson <eparie@bu.edu>
2018-08-07 12:16:59 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,10 +3,11 @@ class Song < ActiveRecord::Base
   accepts_nested_attributes_for :artist
 
   def artist_name
-     self.artist 
+     self.artist
   end
 
   def artist_name=(name)
+    binding.pry
     self.artist ? self.artist.name : Artist.find_or_create_by(name: name)
   end
 
